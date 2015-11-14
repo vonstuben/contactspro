333d3a0fb9bb834bc3307fbf1752c6f27f996d56
vonstuben <vgarbutt@gmail.com>
2015-11-14 15:14:41 -0600

    Factory Pattern
    Added Classes and Interfaces for the Factory Pattern in use.

---
+++ app/src/main/java/com/project/vanswa/jsonfactory/Data.java
@@ -0,0 +1,8 @@
+package com.project.vanswa.jsonfactory;
+
+/**
+ * Created by vonstuben on 11/14/2015.
+ */
+public interface Data {
+
+}
---
+++ app/src/main/java/com/project/vanswa/jsonfactory/DataFactory.java
@@ -0,0 +1,22 @@
+package com.project.vanswa.jsonfactory;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by vonstuben on 11/14/2015.
+ */
+public class DataFactory {
+    ArrayList<Json> dataList = new ArrayList<Json>();
+
+    public Json getData(){
+
+
+        return null;
+    }
+
+    public ArrayList<Json> getDataList(){
+        return dataList;
+    }
+}
+
